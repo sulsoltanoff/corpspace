c10ece8af18a5930e5b45f092a928aa58da326fe
Sultan Soltanov <soltanoff@tuta.io>
2023-02-06 23:05:02 +0500

    Add global consts

---
+++ src/Services/ChatSpace/ChatSpace.Domain/Constants/Constants.cs
@@ -0,0 +1,26 @@
+#region Corpspace© Apache-2.0
+// Copyright 2023 The Corpspace Technologies
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+namespace ChatSpace.Domain.Constants;
+
+public static class Constants
+{
+    // Regex for acceptable logins
+    public const string LoginRegex = "^[_.@A-Za-z0-9-]*$";
+    public const string SystemAccount = "system";
+    public const string AnonymousUser = "anonymoususer";
+    public const string DefaultLangKey = "en";
+}
---
+++ src/Services/ChatSpace/ChatSpace.Domain/Constants/RolesConstants.cs
@@ -0,0 +1,24 @@
+#region Corpspace© Apache-2.0
+// Copyright 2023 The Corpspace Technologies
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+namespace ChatSpace.Domain.Constants;
+
+public static class RolesConstants
+{
+    public const string Admin = "ROLE_ADMIN";
+    public const string User = "ROLE_USER";
+    public const string Anonymous = "ROLE_ANONYMOUS";
+}
