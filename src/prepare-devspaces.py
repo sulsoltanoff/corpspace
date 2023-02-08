bc74297cbdfb57bbf1527d9e2a6b80646f19a954
Sultan Soltanov <soltanoff@tuta.io>
2023-02-08 23:02:05 +0500

    Add const for file name

--- src/prepare-devspaces.py
+++ src/prepare-devspaces.py
@@ -1,5 +1,8 @@
 import os
 
+apps = "app.yaml"
+inf = "inf.yaml"
+
 
 def copy_files(src_dirs, dest_dir, file_name):
     src_file = os.path.join(src_dirs, file_name)
@@ -12,23 +15,23 @@ def copy_files(src_dirs, dest_dir, file_name):
 
 src_dir = "../deploy/k8s/helm"
 
-copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/apigw", "app.yaml")
-copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/apigw", "inf.yaml")
+copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/apigw", apps)
+copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/apigw", inf)
 
 # copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Web/apigw", "app.yaml")
 # copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Web/apigw", "inf.yaml")
 
-copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/aggregator", "app.yaml")
-copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/aggregator", "inf.yaml")
+copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/aggregator", apps)
+copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Mobile/aggregator", inf)
 
-copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Web/aggregator", "app.yaml")
-copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Web/aggregator", "inf.yaml")
+copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Web/aggregator", apps)
+copy_files(src_dir, "./ApiGateways/Corpspace.Bff.Web/aggregator", inf)
 
-copy_files(src_dir, "./Services/Identity/Identity.API", "app.yaml")
-copy_files(src_dir, "./Services/Identity/Identity.API", "inf.yaml")
+copy_files(src_dir, "./Services/Identity/Identity.API", apps)
+copy_files(src_dir, "./Services/Identity/Identity.API", inf)
 
-copy_files(src_dir, "./Services/Payment/Payment.API", "app.yaml")
-copy_files(src_dir, "./Services/Payment/Payment.API", "inf.yaml")
+copy_files(src_dir, "./Services/Payment/Payment.API", apps)
+copy_files(src_dir, "./Services/Payment/Payment.API", inf)
 
-copy_files(src_dir, "./Services/Webhooks/Webhooks.API", "app.yaml")
-copy_files(src_dir, "./Services/Webhooks/Webhooks.API", "inf.yaml")
+copy_files(src_dir, "./Services/Webhooks/Webhooks.API", apps)
+copy_files(src_dir, "./Services/Webhooks/Webhooks.API", inf)
