dbf1dfe68d3b7bc4615588328c95b394371af6e3
Sultan Soltanov <soltanoff@tuta.io>
2023-02-10 23:01:30 +0500

    Delete constructor

--- src/Services/ChatSpace/ChatSpace.Domain/Entities/Messages/Message.cs
+++ src/Services/ChatSpace/ChatSpace.Domain/Entities/Messages/Message.cs
@@ -27,31 +27,6 @@ namespace ChatSpace.Domain.Entities.Messages;
 [Table($"{GeneralConstants.ServiceName}_Message")]
 public class Message : Entity<Guid>, IHasModificationTime
 {
-    public Message(Guid userId, Guid channelId, Guid originalId, bool isPinned, 
-        string content, string type, Dictionary<string, object> props, string hashtags, 
-        List<string> fileIds, bool hasReactions, long replyCount, List<ChatUser> participants, 
-        bool? isFollowing, Metadata metadata, DateTime modificationAt, DateTime creationAt, DateTime? deletionAt, bool isDeleted)
-    {
-        UserId = userId;
-        ChannelId = channelId;
-        OriginalId = originalId;
-        IsPinned = isPinned;
-        Content = content;
-        Type = type;
-        Props = props;
-        Hashtags = hashtags;
-        FileIds = fileIds;
-        HasReactions = hasReactions;
-        ReplyCount = replyCount;
-        Participants = participants;
-        IsFollowing = isFollowing;
-        Metadata = metadata;
-        ModificationAt = modificationAt;
-        CreationAt = creationAt;
-        DeletionAt = deletionAt;
-        IsDeleted = isDeleted;
-    }
-
     [Required]
     public Guid UserId { get; set; }
     
@@ -91,4 +66,6 @@ public class Message : Entity<Guid>, IHasModificationTime
     public DateTime? DeletionAt { get; set; }
     
     public bool IsDeleted { get; set; }
+    
+    public bool IsRead { get; set; }
 }
